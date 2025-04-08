3726dead8a86bf91c1f9773bcbf442f6ab44ba27
Alex Hirschfeld <alex@d-qoi.com>
2025-04-07 22:19:39 -0500

    Working on alerts

--- components/qsgrc/alerts/core.py
+++ components/qsgrc/alerts/core.py
@@ -8,35 +8,35 @@ from qsgrc.messages import AlertMessage, AlertConditions
 logger = get_logger("alerts")
 
 
-MonitorConditions = Tuple[AlertConditions, bool, Any]
+MonitorConditions = tuple[AlertConditions, bool, float | str]
 
 
 class MonitorAlerts:
     def __init__(
-        self, name: str, out_stream: Queue, in_stream: Optional[Queue] = None
+        self, name: str, out_stream: Queue[AlertMessage], in_stream: Queue[tuple[str, float|str]] | None = None
     ) -> None:
-        self.name = name
-        self.out_stream = out_stream
-        self.in_stream = in_stream
-        self.rules: Dict[str, MonitorConditions] = {}
-        self.alert_conditions: Dict[str, bool] = {}
+        self.name: str = name
+        self.out_stream: Queue[AlertMessage] = out_stream
+        self.in_stream: Queue[tuple[str, float|str]] | None = in_stream
+        self.rules: dict[str, MonitorConditions] = {}
+        self.alert_conditions: dict[str, bool] = {}
 
     def add_rule(
         self,
         listen_to: str,
         condition: AlertConditions,
-        threshold: Any,
+        threshold: float | str,
         hold: bool = True,
     ) -> None:
         self.rules[listen_to] = (condition, hold, threshold)
 
     def remove_rule(self, listen_to: str) -> None:
-        self.rules.pop(listen_to)
+        del self.rules[listen_to]
 
     def reset_alert_conditions(self) -> None:
         self.alert_conditions.clear()
 
-    async def __send_alert_update(self, listen_to: str, value: Any) -> None:
+    async def __send_alert_update(self, listen_to: str, value: float | str) -> None:
         await self.out_stream.put(
             AlertMessage(
                 self.name,
@@ -46,7 +46,7 @@ class MonitorAlerts:
             )
         )
 
-    async def check(self, listen_to: str, value: Any) -> None:
+    async def check(self, listen_to: str, value: float | str) -> None:
         if listen_to not in self.rules:
             return
 
